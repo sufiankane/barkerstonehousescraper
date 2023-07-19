648f5a99ebc30c8089695da6561636a76b6e5fdc
Sufyan Khan <sufiankane@gmail.com>
2023-07-19 10:46:46 +0100

    updating html with search box

---
+++ htmlbuilder.py
@@ -0,0 +1,3 @@
+'<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">'
+'<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script> \n'
+'<script src="/js/my.js"></script> \n'
